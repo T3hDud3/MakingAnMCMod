34e59ef6212648b22599d3f8e545bfcd434ae7e8
Eli Marcoux <marcoux.eli@gmail.com>
2014-07-28 13:30:51 -0400

    Set up file structure, mcmod.info, backend stuff

--- src/main/java/com/T3hDud3/MakingAnMCMod/MakingAnMCMod.java
+++ src/main/java/com/T3hDud3/MakingAnMCMod/MakingAnMCMod.java
@@ -1,8 +1,31 @@
 package com.T3hDud3.MakingAnMCMod;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid="MakingAnMCMod", name="Making An MC Mod", version="1.7.2-1.0")
 
 public class MakingAnMCMod {
+    @Mod.Instance("MakingAnMCMod")
+    public static MakingAnMCMod instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLPostInitializationEvent event)
+    {
+
+    }
 }
